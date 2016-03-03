123b3d9e756728aa45407b8873bf3443ed0b4f30
mirceaciu <mirceaciu@gmail.com>
2016-03-03 20:49:33 +0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name transportApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the transportApp
+ */
+angular.module('transportApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
