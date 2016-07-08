5f3d7417fe56fed7a5f90877bebb7fb085092b82
Stanciu Bogdan Mircea <mirceaciu@gmail.com>
2016-07-09 01:30:41 +0300

    MAJOR: change to own data

--- app/scripts/controllers/mapcontrols.js
+++ app/scripts/controllers/mapcontrols.js
@@ -33,45 +33,37 @@ angular.module('transportApp')
   });
 
   /* HARD CODED CARS */
-  $scope.routeOptions = ['1', '101', '102', '103', '104', '105', '106', '11', '112', '116', '117', '122', '123', '124', '125', '126', '131', '133', '135', '136', '137', '138', '139', '14', '141', '143', '149', '16', '162', '163', '168', '173', '178', '182', '21', '220', '221', '232', '246', '25', '253', '261', '268', '27', '282', '300', '301', '302', '304', '311', '312', '313', '32', '323', '330', '331', '335', '336', '35', '36', '368', '381', '385', '40', '409', '41', '42', '422', '427', '438', '44', '45', '453', '455', '46', '47', '5', '56', '601', '61', '62', '634', '65', '655', '656', '66', '668', '682', '69', '697', '7', '70', '780', '783', '79', '8', '85', '86', '90', '91', '93', '96', 'N101', 'N102', 'N103', 'N104', 'N105', 'N106', 'N107', 'N108', 'N109', 'N110', 'N111', 'N112', 'N113', 'N114', 'N115', 'N116', 'N117', 'N118', 'N119', 'N120', 'N121', 'N122', 'N123', 'N124', 'N125'];
-
+  $scope.routeOptions = ['102-t', '104-t', '105-t', '116-r', '116-t', '123-r', '123-t', '124-r', '133-r', '135-t', '137-t', '138-t', '143-r', '143-t', '149-t', '168-t', '178-t', '182-t', '185-r', '185-t', '202-t', '222-r', '222-t', '223-t', '227-r', '227-t', '232-r', '232-t', '236-r', '236-t', '243-r', '268-t', '282-t', '303-r', '313-r', '313-t', '323-r', '323-t', '336-r', '381-r', '381-t', '611-r', '668-r', '682-r'];
 
   // DEFINE 'ROUTE' BUTTON
   $scope.routeRequest = function(){
     //$scope.masina.masina = $scope.car.name[0];
-    $http.get('data/masina-'+ $scope.masina.masina +'.json').success(function(route) {
+    $http.get('data/bus/bus-'+ $scope.masina.masina +'.json').success(function(route) {
       $scope.routeData = route;
 
       //TRANSPORT URBAN DATA - scrap after building new data
-      //get directions number
-      $scope.directionsCount = route.data.length; /*tur-retur*/
-      $scope.directionsList =[];
-      for(var d = 0; d < $scope.directionsCount; d++ ){
-        $scope.directionsList.push(d);
-      }
 
-    //get route car's name from view
-    $scope.currentRoute = route.data[$scope.masina.ruta];
 
     //get middle route for centering
-    $scope.middleRoute = Math.round($scope.currentRoute.stops.length / 2) ;
+    $scope.middleRoute = Math.round($scope.routeData.opriri.length / 2) ;
 
     $scope.center= {
-      lat: Number($scope.currentRoute.stops[$scope.middleRoute].latitude),
-      lng: Number($scope.currentRoute.stops[$scope.middleRoute].longitude),
+      lat: Number($scope.routeData.opriri[$scope.middleRoute].lat),
+      lng: Number($scope.routeData.opriri[$scope.middleRoute].long),
       zoom: 12,
     };
 
     //get all paths of route
     $scope.pathCoordinates = [];
 
-    for(var i = 0; i < $scope.currentRoute.stops.length; i++ ){
-      $scope.pathCoordinates.push({lat: Number($scope.currentRoute.stops[i].latitude), lng: Number($scope.currentRoute.stops[i].longitude)});
+    for(var i = 0; i < $scope.routeData.opriri.length; i++ ){
+      $scope.pathCoordinates.push({lat: Number($scope.routeData.opriri[i].lat), lng: Number($scope.routeData.opriri[i].long)});
     }
+
     //set color for direction
-    if($scope.currentRoute.direction === '0' ){
+    if($scope.routeData.direction === '0' ){
       $scope.routeColor = '#0066ff';
-    }else if ($scope.currentRoute.direction === '1'){
+    }else if ($scope.routeData.direction === '1'){
       $scope.routeColor = '#FF4500';
     }
 
@@ -89,15 +81,14 @@ angular.module('transportApp')
 
   $scope.customRouteRequest = function(){
     console.log('voi incarca geometrie');
-
     $http.get('data/restrict-ignore/masina-131.geojson').success(function(route) {
       $scope.routeData = route;
       console.log('am geometrie');
       //get route car's name from view
-      $scope.currentRoute = $scope.routeData;
+      $scope.routeData = $scope.routeData;
 
       //get all paths of route
-      $scope.pathCoordinates = $scope.currentRoute.coordinates;
+      $scope.pathCoordinates = $scope.routeData.coordinates;
 
       $scope.paths = {
         p1: {
@@ -109,7 +100,8 @@ angular.module('transportApp')
   });
 
 
-
 }; //custom route handler
+
+
 
 }]);
